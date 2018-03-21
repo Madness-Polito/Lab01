db3ef1fe9dd95ba882bf42319afaa072cbb9fe38
Alex Cucco <alex.cucc@hotmail.it>
2018-03-21 22:26:32 +0100

    Added persistency Fixed bug in showActivity: data was not saved when switching between landscape and portrait

--- app/src/main/java/mad/lab1/editProfile.java
+++ app/src/main/java/mad/lab1/editProfile.java
@@ -1,25 +1,42 @@
 package mad.lab1;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-public class editProfile extends AppCompatActivity {
+import com.vansuita.pickimage.bean.PickResult;
+import com.vansuita.pickimage.bundle.PickSetup;
+import com.vansuita.pickimage.dialog.PickImageDialog;
+import com.vansuita.pickimage.listeners.IPickResult;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class editProfile extends AppCompatActivity implements View.OnClickListener, IPickResult{
 
     private EditText name, mail, bio;
     private ImageView pic;
     private ImageButton imgBtn;
+    private Globals g;
+    private PickSetup setup;
+    private final String PREFS_NAME = "MAD_Lab1_prefs";
+    private final String PIC_FILE   = "MAD_Lab1_pic";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        g = (Globals)getApplication();
         setContentView(R.layout.activity_edit_profile);
 
         name = findViewById(R.id.editTextName);
@@ -28,50 +45,96 @@ public class editProfile extends AppCompatActivity {
         pic  = findViewById(R.id.editImageProfile);
         imgBtn = findViewById(R.id.selectImage);
 
+        //set listeners
+        imgBtn.setOnClickListener(this);
+
+        //customize PickSetup (gallery/camera)
+        setup = new PickSetup().setSystemDialog(true);
+
+        //Intent i = getIntent();
+        name.setText(g.getName());
+        mail.setText(g.getMail());
+        bio.setText(g.getBio());
+        pic.setImageBitmap(g.getBmp());
+        /*Bundle b = i.getExtras();
+        name.setText(b.getString("name"));
+        mail.setText(b.getString("mail"));
+        bio.setText(b.getString("bio"));
+        Bitmap bm = b.getParcelable("pic");
+        pic.setImageBitmap(bm);*/
     }
 
     // create the edit bar next to the app name
     // all icons under menu folder are automatically put
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.profile_edit, menu);
+        getMenuInflater().inflate(R.menu.save_profile, menu);
         return true;
     }
 
-    // associate event listener to the edit bar
+    // save data persistently
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
 
-        Intent i = new Intent(getApplicationContext(), editProfile.class);
+        // save data
+        try {
+            // save simple data through sharedPreferences
+            SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+            editor.putString("name", name.getText().toString());
+            editor.putString("mail", mail.getText().toString());
+            editor.putString("bio", bio.getText().toString());
+            editor.apply();
+
+            // save pic to file
+            Bitmap bmp = ((BitmapDrawable)pic.getDrawable()).getBitmap();
+            if (bmp != null){
+                FileOutputStream outStream = openFileOutput(PIC_FILE, Context.MODE_PRIVATE);
+                bmp.compress(Bitmap.CompressFormat.JPEG, 30, outStream);
+                outStream.close();
+            }
+
+            // update global vars
+            g.setName(name.getText().toString());
+            g.setMail(mail.getText().toString());
+            g.setBio(bio.getText().toString());
+            g.setBmp(bmp);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // restart showProfile activity passing saved data
+        Intent i = new Intent(getApplicationContext(), showProfile.class);
         startActivity(i);
+        finish();
 
         return super.onOptionsItemSelected(item);
     }
 
-    // save user profile in Bundle
     @Override
-    public void onSaveInstanceState(Bundle b) {
-
-        b.putString("name", name.getText().toString());
-        b.putString("mail", mail.getText().toString());
-        b.putString("bio", bio.getText().toString());
-        Bitmap bm = pic.getDrawingCache();
-        b.putParcelable("pic", bm);
-
-        super.onSaveInstanceState(b);
+    public void onPickResult(PickResult r) {
+        if (r.getError() == null) {
+            Bitmap bmp = r.getBitmap();
+            pic.setImageBitmap(bmp);
+            g.setBmp(bmp);
+        } /*else {
+            //Handle possible errors
+            //TODO: do what you have to do with r.getError();
+        }*/
     }
 
-    // restore data stored in bundle
     @Override
-    public void onRestoreInstanceState(Bundle b){
-        super.onRestoreInstanceState(b);
+    public void onClick(View view) {
 
-        name.setText(b.getString("name"));
-        mail.setText(b.getString("mail"));
-        bio.setText(b.getString("bio"));
-        Bitmap bm = b.getParcelable("pic");
-        pic.setImageBitmap(bm);
+        //figure out what button ha been pressed
+        switch (view.getId()){
+
+            case R.id.selectImage:
+                //Button to edit image has been pressed
+                PickImageDialog.build(setup).show(this);
+                break;
+        }
     }
+
 
 
 
--- app/src/main/java/mad/lab1/showProfile.java
+++ app/src/main/java/mad/lab1/showProfile.java
@@ -1,24 +1,72 @@
 package mad.lab1;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
 
 public class showProfile extends AppCompatActivity {
 
+    private Globals g;
+    private TextView name, mail, bio;
+    private ImageView pic;
+    private ImageButton imgBtn;
+    private final String PREFS_NAME = "MAD_Lab1_prefs";
+    private final String PIC_FILE   = "MAD_Lab1_pic";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        g = (Globals)getApplication();
+
+        // set up view & references
         setContentView(R.layout.activity_show_profile);
+        name = findViewById(R.id.showTextName);
+        mail = findViewById(R.id.showTextMail);
+        bio = findViewById(R.id.showTextBio);
+        pic = findViewById(R.id.showImageProfile);
+        imgBtn = findViewById(R.id.selectImage);
+
+        // first app run: load data from storage
+        if (b == null){
+
+            // set global data
+            SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+            g.setName(prefs.getString("name", "name"));
+            g.setMail(prefs.getString("mail", "mail"));
+            g.setBio(prefs.getString("bio", "bio"));
+
+            // load pic if exists
+            File f = new File(getFilesDir().getPath() + "/" + PIC_FILE);
+            if (f.exists()) {
+                Bitmap bmp = BitmapFactory.decodeFile(getFilesDir().getPath() + "/" + PIC_FILE);
+                g.setBmp(bmp);
+            }
+
+        }
+
+        // copy data to screen
+        name.setText(g.getName());
+        mail.setText(g.getMail());
+        bio.setText(g.getBio());
+        pic.setImageBitmap(g.getBmp());
     }
 
     // create the edit bar next to the app name
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
-
-      getMenuInflater().inflate(R.menu.profile_edit, menu);
+      getMenuInflater().inflate(R.menu.edit_profile, menu);
       return true;
     }
 
@@ -28,6 +76,7 @@ public class showProfile extends AppCompatActivity {
 
         Intent i = new Intent(getApplicationContext(), editProfile.class);
         startActivity(i);
+        finish();
 
         return super.onOptionsItemSelected(item);
     }
