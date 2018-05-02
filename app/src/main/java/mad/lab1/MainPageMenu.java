45a054c01a4685d05e2c913eb8be68dca9a4de73
ansaldimatteo <ansaldimatteo@yahoo.co.uk>
2018-05-02 19:11:18 +0200

    Fixed selecting a book from search

--- app/src/main/java/mad/lab1/MainPageMenu.java
+++ app/src/main/java/mad/lab1/MainPageMenu.java
@@ -3,6 +3,7 @@ package mad.lab1;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Parcelable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
@@ -341,18 +342,17 @@ public class MainPageMenu extends AppCompatActivity {
     }
 
     private void showBookInfo(String isbn) {
-        //TODO: Make seachedBook into ISBNInfo, not Book.
 
         dbRef = FirebaseDatabase.getInstance().getReference().child("isbn").child(isbn);
 
         dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
-                Book searchedBook = snapshot.getValue(Book.class);
+                IsbnInfo searchedBook = snapshot.getValue(IsbnInfo.class);
                 Bundle arg = new Bundle();
                 arg.putParcelable("book", searchedBook);
                 Intent i = new Intent(getApplicationContext(), ShowSelectedBookInfo.class);
-                i.putExtra("argument", searchedBook);
+                i.putExtra("argument", (Parcelable) searchedBook);
                 startActivity(i);
 
             }
