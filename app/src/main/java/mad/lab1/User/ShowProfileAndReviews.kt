36482972527de8fbad0018132ebf231c319f514a
MaizeDaniele <danielecaschili@tiscali.it>
2018-05-30 13:22:49 +0200

    Reviews List connected Solved Minor bugs

--- app/src/main/java/mad/lab1/User/ShowProfileAndReviews.kt
+++ app/src/main/java/mad/lab1/User/ShowProfileAndReviews.kt
@@ -1,21 +1,79 @@
 package mad.lab1.User
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import mad.lab1.Database.UserInfo
 import mad.lab1.R
+import mad.lab1.review.ReviewsActivity
+import org.w3c.dom.Text
 
 class ShowProfileAndReviews : AppCompatActivity() {
 
     private lateinit var toolbar: Toolbar
+    private lateinit var name : TextView
+    private lateinit var city : TextView
+    private lateinit var birthDate : TextView
+    private lateinit var email : TextView
+    private lateinit var phone : TextView
+    private lateinit var image : ImageView
+    private lateinit var bookBorrowedNumber : TextView
+    private lateinit var showMoreReviews : ImageView
+    private lateinit var showMoreReviewsText : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile_and_reviews)
-        initializeToolbar()
+        initialization()
     }
 
+
+
+    private fun initialization(){
+
+        name = findViewById(R.id.showRequestTextName)
+        city = findViewById(R.id.showTextCityStateName)
+        birthDate = findViewById(R.id.showRequestTextBirthDate)
+        email = findViewById(R.id.showRequestTextMail)
+        phone = findViewById(R.id.showRequestTextTelephone)
+        image = findViewById(R.id.showRequestImageProfile)
+        bookBorrowedNumber = findViewById(R.id.showRequestBookBorrowed)
+        showMoreReviews = findViewById(R.id.showMoreReviews)
+        showMoreReviewsText = findViewById(R.id.textView6)
+
+        var bundle = intent.getBundleExtra("user")
+        var u : UserInfo = bundle.getParcelable("user")
+
+        initializeToolbar()
+
+        name.text = u.name
+        city.text = u.city
+        birthDate.text = u.dob
+        email.text = u.mail
+        phone.text = u.phone
+
+
+        showMoreReviews.setOnClickListener{
+            var i = Intent(this, ReviewsActivity::class.java)
+            i.putExtra("uid", u.uid)
+            startActivity(i)
+        }
+
+        showMoreReviewsText.setOnClickListener{
+            var i = Intent(this, ReviewsActivity::class.java)
+            i.putExtra("uid", u.uid)
+            startActivity(i)
+        }
+
+
+        //TODO Set image, set number of borrowed books
+
+
+    }
 
 
     private fun initializeToolbar(){
--- app/src/main/java/mad/lab1/review/ReviewsActivity.kt
+++ app/src/main/java/mad/lab1/review/ReviewsActivity.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.view.View
 import com.google.firebase.database.*
 import mad.lab1.R
 import mad.lab1.User.Authentication
@@ -15,18 +17,22 @@ class ReviewsActivity : AppCompatActivity() {
     private var dbRef: DatabaseReference? = null
     private var reviewListRef: DatabaseReference? = null
     private var reviewListener: ChildEventListener? = null
+    private var toolbar : Toolbar? = null
+    private var rv : RecyclerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.reviews_layout)
 
+        initializeToolbar()
+
         // get uid of user of which we want to get the reviews from intent
         val uid = intent.getStringExtra("uid")
 
         // init layout
-        val rv = findViewById<RecyclerView>(R.id.reviewList)
-        rv.layoutManager = LinearLayoutManager(this)
-        rv.adapter = ReviewsAdapter(reviews, this)
+        rv = findViewById(R.id.reviewList)
+        rv?.layoutManager = LinearLayoutManager(this)
+        rv?.adapter = ReviewsAdapter(reviews, this)
 
         // get firebase reference for reading reviews
         dbRef = FirebaseDatabase.getInstance()
@@ -40,9 +46,13 @@ class ReviewsActivity : AppCompatActivity() {
             override fun onChildAdded(dataSnapshot: DataSnapshot?, previousChildName: String?) {
                 val review = dataSnapshot!!.getValue(Review::class.java)
                 reviews.add(review!!)
+                rv?.adapter?.notifyItemInserted(reviews.indexOf(review))
             }
 
             override fun onChildChanged(dataSnapshot: DataSnapshot?, previousChildName: String?) {
+
+                //TODO if review modified, change here the view
+
             }
 
             override fun onChildRemoved(dataSnapshot: DataSnapshot?) {
@@ -57,13 +67,28 @@ class ReviewsActivity : AppCompatActivity() {
 
     }
 
-    override fun onRestart() {
-        super.onRestart()
+
+    private fun initializeToolbar(){
+        toolbar = findViewById<Toolbar>(R.id.requestBookListToolbar)
+        toolbar?.setNavigationIcon(R.drawable.ic_arrow_back_white_32dp)
+
+
+        setSupportActionBar(toolbar)
+
+        toolbar?.setNavigationOnClickListener(View.OnClickListener { onBackPressed() })
+
+    }
+
+    override fun onResume() {
+        super.onResume()
         reviewListRef?.addChildEventListener(reviewListener)
     }
 
     override fun onPause() {
         super.onPause()
         reviewListRef?.removeEventListener(reviewListener)
+        var size = reviews.size
+        reviews.clear()
+        rv?.adapter?.notifyItemRangeChanged(0, size)
     }
 }
